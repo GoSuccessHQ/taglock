3b137160ca192f7d9407e5d482cf9b3144f2c433
Paul Gossen <paul@gosuccess.io>
2026-01-02 20:01:43 +0100

    Remove shortcode attribute validation from PHP

--- src/Service/ShortcodeService.php
+++ src/Service/ShortcodeService.php
@@ -21,9 +21,6 @@ use function implode;
 use function array_map;
 use function current_user_can;
 use function function_exists;
-use function array_diff;
-use function array_keys;
-use function count;
 use function trim;
 
 /**
@@ -55,17 +52,6 @@ final class ShortcodeService {
 
 		// Ensure frontend assets are available for this shortcode instance.
 		$this->assetService->enqueueFrontendAssets( true );
-
-		// Strict: only `id` is supported.
-		if ( is_array( $atts ) ) {
-			$extraKeys = array_diff( array_keys( $atts ), [ 'id' ] );
-			if ( count( $extraKeys ) > 0 ) {
-				$this->logger->warning( __( 'TagLock shortcode uses unsupported attributes', 'taglock' ), [
-					'attributes' => $extraKeys,
-				] );
-				return '<div class="taglock-error">' . esc_html__( 'Error: Only the id attribute is supported.', 'taglock' ) . '</div>';
-			}
-		}
 
 		$attributes = shortcode_atts(
 			[
