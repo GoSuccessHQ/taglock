edd44c1aa380f50a5c671173e62cfd93d2aeba45
Paul Gossen <paul@gosuccess.io>
2026-01-03 12:58:22 +0100

    fix(react): Fix remaining settings references in useCallback dependencies

--- assets/build/admin/index.asset.php
+++ assets/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'bfb091f183ba65c32a24');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5c4d8169b9f930ade613');
